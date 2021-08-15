6640121d65c1dcbcbc3f3694fb34a8273593ae3a
Antoniocns <acnspeixe@gmail.com>
2021-08-14 22:18:07 -0300

    acabou

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AsciiArt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(" ||  || ");
+            Console.WriteLine(" \\()// ");
+            Console.WriteLine("//(__)\\");
+            Console.WriteLine("||    ||");
+            Console.ResetColor();
+        }
+    }
+}
